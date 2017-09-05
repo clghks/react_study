a76ecf71a1efe78d5ed1bf54c77625aacc7e5fcb
Chihwan <clghks@gmail.com>
2017-09-05 15:27:10 +0900

    연락처 선택 기능 추가

--- Contact/src/components/Contact.js
+++ Contact/src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails'
 
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       keyword: '',
+      selectedKey: -1,
       contactData: [{
         name: 'Abet', phone: '010-0000-0001'
       },{
@@ -18,20 +20,31 @@ export default class Contact extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
     this.setState({
       keyword: e.target.value
-    })
+    });
+  }
+
+  handleClick(key) {
+    this.setState({selectedKey: key});
+
+    console.log(key, 'is Selected');
   }
 
   render() {
     const mapToComponents = (data) => {
       data.sort();
-      data = data.filter((contact) => {return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1})
+      data = data.filter((contact) => {
+        return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1;
+      });
       return data.map((contact, i) => {
-        return (<ContactInfo contact={contact} key={i} />);
+        return (
+          <ContactInfo contact={contact} key={i} onClick={() => this.handleClick(i)} />
+        );
       });
     };
 
@@ -40,6 +53,8 @@ export default class Contact extends React.Component {
         <h1>Contacts</h1>
         <input name="keyword" placeholder="Search" value={this.state.keyword} onChange={this.handleChange}/>
         <div>{mapToComponents(this.state.contactData)}</div>
+
+        <ContactDetails isSelected={this.state.selectedKey != -1} contact={this.state.contactData[this.state.selectedKey]}/>
       </div>
     );
   }
